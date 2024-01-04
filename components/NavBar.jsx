8688895a7f266704c9f995f3012fd7b693bc5632
Abideen Shittu <misterjrshittu@gmail.com>
2024-01-04 09:48:00 +0100

    Add Nav

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 
+const navIcons = [
+    {src: "/assets/icons/search.svg", alt: "search"},
+    {src: "/assets/icons/black-heart.svg", alt: "heart"},
+    {src: "/assets/icons/user.svg", alt: "user"}
+]
+
 const NavBar = () => {
   return (
     <header className='w-full'>
@@ -17,6 +23,21 @@ const NavBar = () => {
                 Ghost<span className='text-primary'>Finder</span>
             </p>
         </Link>
+
+        <div className='flex items-center gap-5'>
+            {
+                navIcons.map((icon) => (
+                    <Image 
+                        src={icon.src}
+                        key={icon.alt}
+                        alt={icon.alt}
+                        width={28}
+                        height={28}
+                        className='object-fit'
+                    />
+                ))
+            }
+        </div>
       </nav>
     </header>
   )
